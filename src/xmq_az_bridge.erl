d470d14dc4638860f6a9ac8d1eecc8538747a6a2
garrett <g@rre.tt>
2012-03-18 21:55:52 -0500

    Basic bridging

--- src/xmq_az_bridge.erl
+++ src/xmq_az_bridge.erl
@@ -30,13 +30,15 @@ start_link(Bindings, Options) ->
 
 init([Bindings, Options]) ->
     start_amqp_connect(Options),
-    ZmqContext = init_zmq_router(Options),
-    {ok, init_state(Bindings, ZmqContext)}.
+    {ZmqContext, ZmqRouter} = init_zmq_router(Options),
+    {ok, init_state(Bindings, ZmqContext, ZmqRouter)}.
 
 handle_msg({amqp_connect, Connection}, _From, State) ->
     handle_amqp_connect(Connection, State);
-handle_msg({zmq_bind, Router}, _From, State) ->
-    handle_zmq_router(Router, State);
+handle_msg({#'basic.deliver'{routing_key=Key}, Msg}, _From, State) ->
+    handle_amqp_msg(Key, Msg, State);
+handle_msg({zmq, _Router, Client, [rcvmore]}, _From, State) ->
+    handle_zmq_msg(Client, recv_zmq_parts([]), State);
 handle_msg({'EXIT', _Pid, normal}, _From, State) ->
     %% Linked tasks used for connections
     {noreply, State};
@@ -53,19 +55,20 @@ terminate(_Reason, State) ->
     close_amqp_connection(State).
 
 %%%===================================================================
-%%% Internal functions
+%%% Internal init
 %%%===================================================================
 
-init_state(Bindings, ZmqContext) ->
+init_state(Bindings, ZmqContext, ZmqRouter) ->
     #state{bindings=Bindings,
            zcontext=ZmqContext,
+           zrouter=ZmqRouter,
            routes=new_routes()}.
 
 init_zmq_router(Options) ->
     {ok, Context} = erlzmq:context(),
     {ok, Router} = erlzmq:socket(Context, [router, {active, true}]),
     ok = erlzmq:bind(Router, router_endpoint(Options)),
-    Context.
+    {Context, Router}.
 
 new_routes() ->
     {ok, Routes} = xmq_routes:start_link(),
@@ -80,14 +83,14 @@ amqp_params(Options) ->
 router_endpoint(Options) ->
     proplists:get_value(router_endpoint, Options, ?DEFAULT_ROUTER_ENDPOINT).
 
+%%%===================================================================
+%%% Internal AMQP setup
+%%%===================================================================
+
 handle_amqp_connect(Connection, #state{bindings=Bindings}=State) ->
     e2_log:info(az_bridge_amqp_connected),
     setup_amqp_queue(Connection, Bindings),
     {noreply, State#state{aconn=Connection}}.
-
-handle_zmq_router(Router, State) ->
-    e2_log:info(az_bridge_zmq_bound),
-    {noreply, State#state{zrouter=Router}}.
 
 setup_amqp_queue(Connection, Bindings) ->
     {Channel, Queue} = create_amqp_queue(Connection),
@@ -111,6 +114,61 @@ add_amqp_bindings([{Exchange, Binding}|Rest], Channel, Queue) ->
     add_amqp_bindings(Rest, Channel, Queue);
 add_amqp_bindings([Binding|Rest], Channel, Queue) ->
     add_amqp_bindings([{<<"amq.topic">>, Binding}|Rest], Channel, Queue).
+
+%%%===================================================================
+%%% Internal AMQP message handling
+%%%===================================================================
+
+handle_amqp_msg(Key, Msg, #state{routes=Routes}=State) ->
+    maybe_send_msg(xmq_routes:get_topic(Routes, Key), Key, Msg, State),
+    {noreply, State}.
+
+maybe_send_msg([], _Key, _Msg, _State) -> ok;
+maybe_send_msg(Clients, Key, Msg, #state{zrouter=Router}) ->
+    send_msg(Router, Clients, Key, term_to_binary(Msg)).
+
+send_msg(_Router, [], _Key, _MsgBin) -> ok;
+send_msg(Router, [Client|Rest], Key, MsgBin) ->
+    erlzmq:send(Router, Client, [sndmore]),
+    erlzmq:send(Router, Key, [sndmore]),
+    erlzmq:send(Router, MsgBin),
+    send_msg(Router, Rest, Key, MsgBin).
+
+%%%===================================================================
+%%% Internal ZMQ message handling
+%%%===================================================================
+
+recv_zmq_parts(Acc) ->
+    receive
+        {zmq, _Router, Part, []} ->
+            lists:reverse([Part|Acc]);
+        {zmq, _Router, Part, [rcvmore]} ->
+            recv_zmq_parts([Part|Acc])
+    after
+        %% TODO: This feels weird - but how to guard against hangs?
+        1000 -> exit(zmq_recv_timeout)
+    end.
+
+handle_zmq_msg(Client, [<<"bind">>|Bindings], State) ->
+    %% TODO: specify expires
+    add_zmq_bindings(Client, Bindings, State),
+    {noreply, State};
+handle_zmq_msg(_Client, Msg, State) ->
+    e2_log:info({unhandled_az_bridge_zmq_msg, Msg}),
+    {noreply, State}.
+
+add_zmq_bindings(Client, Bindings, #state{routes=Routes}) ->
+    handle_add_topic_bindings_result(
+      catch(xmq_routes:add_topic_bindings(Routes, Bindings, Client)),
+      Bindings).
+
+handle_add_topic_bindings_result({'EXIT', {badarg, _}}, Bindings) ->
+    e2_log:error({az_bridge_illegal_bindings, Bindings});
+handle_add_topic_bindings_result(ok, _Bindings) -> ok.
+
+%%%===================================================================
+%%% Internal term / cleanup
+%%%===================================================================
 
 term_zmq_context(#state{zcontext=C}) ->
     e2_log:info(az_bridge_zmq_context_term),
